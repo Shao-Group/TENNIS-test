57e8669cdf8f8ee7c2892ea902777e18d2f8527a
Xiaofei (Carl) Zang <zangx@chop.edu>
2025-12-17 14:01:17 -0500

    Update tennis_expression_plot.py

--- scripts/tennis_expression_plot.py
+++ scripts/tennis_expression_plot.py
@@ -1,3 +1,51 @@
+#!/usr/bin/env python3
+"""
+Plot validated TENNIS isoform expression vs total transcript group expression.
+"""
+
+import argparse
+import glob
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+from collections import defaultdict
+import GTF
+
+
+def parse_gtf(gtf_file):
+    """Parse GTF and compute grouping key (first_exon_end, last_intron_start) per transcript."""
+    transcript_exons = defaultdict(list)
+
+    for fields in GTF.lines(gtf_file):
+        if fields['feature'] != 'exon':
+            continue
+
+        chrom = fields['seqname']
+        start = int(fields['start'])
+        end = int(fields['end'])
+        strand = fields['strand']
+        transcript_id = fields['transcript_id']
+
+        if transcript_id:
+            transcript_exons[transcript_id].append((chrom, start, end, strand))
+
+    # Compute grouping key per transcript
+    transcript_to_group = {}
+    for tid, exons in transcript_exons.items():
+        if len(exons) < 2:
+            # Single exon, use exon boundaries as group key
+            continue
+        else:
+            # Sort exons by position
+            exons_sorted = sorted(exons, key=lambda x: x[1])
+            chrom = exons_sorted[0][0]
+            strand = exons_sorted[0][3]
+            first_exon_end = exons_sorted[0][2]
+            last_intron_start = exons_sorted[-2][2]  # end of second-to-last exon
+            transcript_to_group[tid] = (chrom, first_exon_end, last_intron_start, strand)
+    return transcript_to_group
+
+
 def parse_tmap(tmap_file):
     """Parse tmap file and return validated TENNIS-to-annotation mapping."""
     tennis_to_annot = {}
